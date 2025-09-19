d0d56d91c933c370ae0f6d6d2adfd70c935aaf91
DavidingPlus <davidingplus@qq.com>
2025-09-19 17:34:09 +0800

    done all process

--- scripts/generate-list-data.cjs
+++ scripts/generate-list-data.cjs
@@ -1,26 +1,50 @@
 const fs = require('fs')
 const json2md = require('json2md')
 
-const originTableJson = JSON.parse(fs.readFileSync('scripts/old-list.json', 'utf-8'))
 
-const tableJson = originTableJson.flatMap(table => {
-    if (!table.data || 0 === table.data.length) return []
+/**
+ * 转换 Table 表格数据。将 JSON 表格数据转换成 Markdown 格式的数据并写入到指定文件中。
+ * @param {string} inputJsonPath - 原始 JSON 文件路径,
+ * @param {string} outputMdPath - 输出的 Markdown 文件路径,
+ * @param {string} headerText - 页面开头显示的标题和说明文字,
+ */
+function generateMarkdownFromJson(inputJsonPath, outputMdPath, headerText = '') {
+    // 读取原始 JSON 文件。
+    const originJson = JSON.parse(fs.readFileSync(inputJsonPath, 'utf-8'))
 
-    // 生成带行号的数据。
-    const numberedData = table.data.map((item, idx) => ({ 序号: idx + 1, ...item }))
-    // 表头包括序号。
-    const headers = Object.keys(numberedData[0])
-    // 转成二维数组。
-    const rows = numberedData.map(item => headers.map(h => item[h]))
+    // 转换每张表。
+    const tableJson = originJson.flatMap(table => {
+        if (!table.data || table.data.length === 0) return []
+
+        // 给每行加行号。
+        const numberedData = table.data.map((item, idx) => ({ 序号: idx + 1, ...item }))
+
+        // 表头和行数据。
+        const headers = Object.keys(numberedData[0])
+        const rows = numberedData.map(item => headers.map(h => item[h]))
+
+        return [
+            { h2: table.title },
+            { table: { headers, rows } }
+        ]
+    })
+
+    // 生成 Markdown。
+    const outputMd = `${headerText}\n\n` + json2md(tableJson)
+
+    // 写入文件。
+    fs.writeFileSync(outputMdPath, outputMd, 'utf-8')
+}
 
 
-    return [
-        { h2: table.title },
-        { table: { headers, rows } }
-    ]
-})
+generateMarkdownFromJson(
+    'scripts/old-list.json',
+    'docs/guide/ranking-list/old-list.md',
+    '# 旧榜单\n\n本页面展示的榜单完全来源于原腾讯文档中的内容，原链接[见此](https://docs.qq.com/sheet/DTXNnc09DRGZWVGxt)。'
+)
 
-const tableMd = '# 旧榜单\n\n' + json2md(tableJson)
-console.log(tableMd)
-
-fs.writeFileSync('docs/guide/table-test.md', tableMd, 'utf-8')
+generateMarkdownFromJson(
+    'scripts/new-list.json',
+    'docs/guide/ranking-list/new-list.md',
+    '# 旧榜单\n\n本页面展示的榜单完全来源于原腾讯文档中的内容，原链接[见此](https://docs.qq.com/sheet/DTUhETnNCQ0RoRm9v)。'
+)
