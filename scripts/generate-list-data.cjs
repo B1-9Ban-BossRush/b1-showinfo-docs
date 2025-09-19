36a0f26d6af671ceae992935944e8d31827f9564
DavidingPlus <davidingplus@qq.com>
2025-09-19 18:59:19 +0800

    add all data

--- scripts/generate-list-data.cjs
+++ scripts/generate-list-data.cjs
@@ -12,32 +12,39 @@ function generateMarkdownFromJson(inputJsonPath, outputMdPath, pageHeader = '') 
     // 读取原始 JSON 文件。
     const rawJson = JSON.parse(fs.readFileSync(inputJsonPath, 'utf-8'))
 
-    // 转换每张表。
-    const mdElements = rawJson.flatMap(section => {
-        if (!section.data || section.data.length === 0) return []
-
-        // 给每行加行号。
-        const tableRows = section.data.map((entry, index) => ({
-            排名: 1 + index,
-            ...entry
-        }))
-
-        // 获取列名（表头）。
-        const columnNames = Object.keys(tableRows[0])
-
-        // 每行的数据数组。
-        const tableContent = tableRows.map(row => columnNames.map(col => row[col]))
-
-        return [
-            // 表格标题。
-            { h2: section.title },
-            // 表格内容。         
-            { table: { headers: columnNames, rows: tableContent } }
+    // 转换每个 section。
+    const markdownElements = rawJson.flatMap(section => {
+        const elements = [
+            // 总是生成标题
+            { h2: section.title }
         ]
+
+        if (section.table && section.table.length > 0) {
+            // 给每行加行号
+            const tableRows = section.table.map((entry, index) => ({
+                排名: 1 + index,
+                ...entry
+            }))
+
+            // 获取列名（表头）。
+            const columnNames = Object.keys(tableRows[0])
+
+            // 每行的数据数组。
+            const tableContent = tableRows.map(row => columnNames.map(col => row[col]))
+
+            elements.push({
+                table: {
+                    headers: columnNames,
+                    rows: tableContent
+                }
+            })
+        }
+
+        return elements
     })
 
     // 生成 Markdown 文本。
-    const outputMdText = `${pageHeader}\n\n` + json2md(mdElements)
+    const outputMdText = `${pageHeader}\n\n` + json2md(markdownElements)
 
     // 写入文件。
     fs.writeFileSync(outputMdPath, outputMdText, 'utf-8')
@@ -46,14 +53,14 @@ function generateMarkdownFromJson(inputJsonPath, outputMdPath, pageHeader = '') 
 
 if (module === require.main) {
     generateMarkdownFromJson(
-        'scripts/old-list.json',
-        'docs/guide/ranking-list/old-list.md',
-        '# 旧榜单\n\n本页面展示的榜单完全来源于原腾讯文档中的内容，若更新不及时请优先参考[原文档](https://docs.qq.com/sheet/DTXNnc09DRGZWVGxt)。'
-    )
-
-    generateMarkdownFromJson(
         'scripts/new-list.json',
         'docs/guide/ranking-list/new-list.md',
         '# 新榜单\n\n本页面展示的榜单完全来源于原腾讯文档中的内容，若更新不及时请优先参考[原文档](https://docs.qq.com/sheet/DTUhETnNCQ0RoRm9v)。'
+    )
+
+    generateMarkdownFromJson(
+        'scripts/old-list.json',
+        'docs/guide/ranking-list/old-list.md',
+        '# 旧榜单\n\n本页面展示的榜单完全来源于原腾讯文档中的内容，由于已停更，仅展示前十名，[原文档](https://docs.qq.com/sheet/DTXNnc09DRGZWVGxt)。'
     )
 }
