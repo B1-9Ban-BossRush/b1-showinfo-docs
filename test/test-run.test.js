d36020dc914948ee687636c6d3364a3e0cf902fe
DavidingPlus <davidingplus@qq.com>
2025-09-28 17:57:28 +0800

    update new list total 2025/9/28

--- test/test-run.test.js
+++ test/test-run.test.js
@@ -6,22 +6,22 @@ import 'moment-duration-format';
 
 describe('Calculate Total Score', () => {
     it("计算新榜总成绩", () => {
-        // const scores = ["53''79", "1'26''75", "1'54''15", "2'31''50", "2'14''44", "2'46''08", "1'21''98", "3'03''56", "5'05''88"];
+        const scores = [
+            ["53''79", "1'26''75", "1'54''15", "2'31''50", "2'14''44", "2'46''08", "1'21''98", "3'03''56", "5'05''09"],
+            ["54''52", "1'28''78", "1'53''64", "2'35''04", "2'12''11", "2'40''50", "1'19''93", "3'23''94", "5'03''94"],
+            ["55''92", "1'29''72", "1'56''82", "2'53''17", "2'15''26", "2'35''49", "1'22''82", "3'08''95", "5'01''97"],
+            ["1'10''45", "1'34''19", "2'06''63", "3'14''00", "2'21''33", "2'44''89", "1'20''40", "3'53''20", "5'18''93"]
+        ];
 
-        // const scores = ["54''52", "1'28''78", "1'53''64", "2'35''04", "2'12''11", "2'40''50", "1'19''93", "3'23''94", "5'03''94"];
 
-        const scores = ["55''92", "1'29''72", "1'56''82", "2'53''17", "2'15''26", "2'35''49", "1'22''82", "3'08''95", "5'18''64"];
+        for (const score of scores) {
+            let totalScore = 0;
 
-        let totalScore = 0
+            for (const e of score) {
+                totalScore += parse(normalizeTime(e));
+            }
 
-        for (let i = 0; i < scores.length; ++i) {
-            const e = scores[i];
-
-            totalScore += parse(normalizeTime(e))
+            console.log(moment.duration(totalScore, 'milliseconds').format("mm:ss.SSS", { trim: false }));
         }
-
-        const formattedTotalScore = moment.duration(totalScore, 'milliseconds').format("mm:ss.SSS", { trim: false });
-
-        console.log(formattedTotalScore);
     });
 })
